16f06b39868840a26a7312743a18ec31c009091a
leofernandesmo <leofernandesmo@gmail.com>
2018-02-23 14:22:44 -0300

    fix error in plume install

--- build_script.sh
+++ build_script.sh
@@ -8,7 +8,7 @@ echo "Building aux-exp..."
 #É importante verficiar antes se a pasta do repositorio local ja não esta preenchida...para nao precisar fazer isso novamente.
 mvn deploy:deploy-file -Dfile=lib/mujava.jar -DgroupId=mujava -DartifactId=mujava -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
 
-mvn deploy:deploy-file -Dfile=lib/plume.jar -DgroupId=mujava -DartifactId=plume -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
+mvn deploy:deploy-file -Dfile=lib/plume.jar -DgroupId=plume -DartifactId=plume -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
 
 mvn deploy:deploy-file -Dfile=lib/openjava.jar -DgroupId=openjava -DartifactId=openjava -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
 
@@ -17,9 +17,9 @@ mvn deploy:deploy-file -Dfile=lib/tools.jar -DgroupId=com.sun -DartifactId=tools
 mvn deploy:deploy-file -Dfile=lib/jdollyIf.jar -DgroupId=jdolly -DartifactId=jdolly -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
 
 #Verificar se ja nao existe um nimrod.jar na pasta lib/ e perguntar ao usuario se deseja substituir.
-echo "Downloading Nimrod..."
-wget -P lib/ "https://onedrive.live.com/download?cid=3E9EEBEE0AA4D1F5&resid=3E9EEBEE0AA4D1F5%2149435&authkey=APD2AuoVOL0IUw8" -q --show-progress
-mv "lib/download?cid=3E9EEBEE0AA4D1F5&resid=3E9EEBEE0AA4D1F5%2149435&authkey=APD2AuoVOL0IUw8" "lib/nimrod.jar"
+#echo "Downloading Nimrod..."
+#wget -P lib/ "https://onedrive.live.com/download?cid=3E9EEBEE0AA4D1F5&resid=3E9EEBEE0AA4D1F5%2149435&authkey=APD2AuoVOL0IUw8" -q --show-progress
+#mv "lib/download?cid=3E9EEBEE0AA4D1F5&resid=3E9EEBEE0AA4D1F5%2149435&authkey=APD2AuoVOL0IUw8" "lib/nimrod.jar"
 
 mvn deploy:deploy-file -Dfile=lib/nimrod.jar -DgroupId=nimrod -DartifactId=nimrod -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
 

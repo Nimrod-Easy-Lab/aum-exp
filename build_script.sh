81fbe85c5c972eede264dbce2e992ea420e24bc5
Leonardo Fernandes <leofernandesmo@gmail.com>
2018-03-20 15:07:41 -0300

    added support to evosuite

--- build_script.sh
+++ build_script.sh
@@ -8,7 +8,8 @@ echo "Building aux-exp..."
 #É importante verficiar antes se a pasta do repositorio local ja não esta preenchida...para nao precisar fazer isso novamente.
 mvn deploy:deploy-file -Dfile=lib/mujava.jar -DgroupId=mujava -DartifactId=mujava -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
 
-mvn deploy:deploy-file -Dfile=lib/plume.jar -DgroupId=plume -DartifactId=plume -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
+#mvn deploy:deploy-file -Dfile=lib/plume.jar -DgroupId=plume -DartifactId=plume -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
+mvn install:install-file -Dfile=lib/plume.jar -DgroupId=plume -DartifactId=plume -Dversion=1.0 -Dpackaging=jar
 
 mvn deploy:deploy-file -Dfile=lib/openjava.jar -DgroupId=openjava -DartifactId=openjava -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
 
@@ -21,7 +22,9 @@ mvn deploy:deploy-file -Dfile=lib/jdollyIf.jar -DgroupId=jdolly -DartifactId=jdo
 #wget -P lib/ "https://onedrive.live.com/download?cid=3E9EEBEE0AA4D1F5&resid=3E9EEBEE0AA4D1F5%2149435&authkey=APD2AuoVOL0IUw8" -q --show-progress
 #mv "lib/download?cid=3E9EEBEE0AA4D1F5&resid=3E9EEBEE0AA4D1F5%2149435&authkey=APD2AuoVOL0IUw8" "lib/nimrod.jar"
 
-mvn deploy:deploy-file -Dfile=lib/nimrod.jar -DgroupId=nimrod -DartifactId=nimrod -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
+#mvn deploy:deploy-file -Dfile=lib/nimrod.jar -DgroupId=nimrod -DartifactId=nimrod -Dversion=1.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
+#Install in the user local repository
+mvn install:install-file -Dfile=lib/nimrod.jar -DgroupId=nimrod -DartifactId=nimrod -Dversion=1.0 -Dpackaging=jar
 
 mvn clean
 
